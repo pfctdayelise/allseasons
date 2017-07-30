1c4293c4798371ffc91415bdba1229d5be0a3f55
Brianna Laugher <brianna@laugher.id.au>
2017-07-30 23:11:35 +1000

    Remove fixture from location tests

--- tests/test_location.py
+++ tests/test_location.py
@@ -2,21 +2,16 @@ import pytest
 import location
 
 
-@pytest.fixture
-def places():
-    """
-    Define some lat-lng data for use in location related tests
-    """
-    return {'london': (51.507351, -0.127758),
-            'murmansk': (68.958524, 33.08266),
-            'buenos aires': (-34.603684, -58.381559),
-            'melbourne': (-37.813628, 144.963058),
-            'pontianak': (0.0, -109.20),
-            'mumbai': (19.0760, 72.8777),
-            'delhi': (28.7041, 77.1025),
-            'bangalore': (12.9716, 77.5946),
-            'hyderabad': (17.3850, 78.4867),
-    }
+places = {'london': (51.507351, -0.127758),
+          'murmansk': (68.958524, 33.08266),
+          'buenos aires': (-34.603684, -58.381559),
+          'melbourne': (-37.813628, 144.963058),
+          'pontianak': (0.0, -109.20),
+          'mumbai': (19.0760, 72.8777),
+          'delhi': (28.7041, 77.1025),
+          'bangalore': (12.9716, 77.5946),
+          'hyderabad': (17.3850, 78.4867),
+}
 
 
 class TestLocation:
@@ -24,28 +19,28 @@ class TestLocation:
     to keep the structure parallel with the source class.
     """
 
-    def test_country_uk(self, places):
+    def test_country_uk(self):
         lat, lng = places['london']
         loc = location.Location(lat, lng)
         assert loc.country == 'UK'
 
-    def test_country_russia(self, places):
+    def test_country_russia(self):
         lat, lng = places['murmansk']
         loc = location.Location(lat, lng)
         assert loc.country == 'Russian Federation'
 
-    def test_country_argentina(self, places):
+    def test_country_argentina(self):
         lat, lng = places['buenos aires']
         loc = location.Location(lat, lng)
         assert loc.country == 'Argentina'
 
-    def test_country_australia(self, places):
+    def test_country_australia(self):
         lat, lng = places['melbourne']
         loc = location.Location(lat, lng)
         assert loc.country == 'Australia'
 
-    @pytest.mark.xfail()
-    def test_country_indonesia(self, places):
+    @pytest.mark.xfail(reason="Not sure why this fails")
+    def test_country_indonesia(self):
         lat, lng = 'pontianak'
         loc = location.Location(lat, lng)
         assert loc.country == 'Indonesia'
@@ -57,7 +52,7 @@ class TestLocation:
         ('melbourne', 'southern'),
         ('pontianak', 'southern'),
     ])
-    def test_hemisphere(self, places, placename, expected):
+    def test_hemisphere(self, placename, expected):
         lat, lng = places[placename]
         loc = location.Location(lat, lng)
         assert loc.hemisphere == expected
